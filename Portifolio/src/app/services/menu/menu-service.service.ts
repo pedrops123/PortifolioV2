2b100ce651da22d99df08992f9211f2d611e61d1
pedrops123 <pedro.furlan1304@hotmail.com>
2021-05-19 14:26:53 -0300

    Finalizacao layout meus trabalhos | Correcao layout contato | Inclusao de foto perfil portifolio e ajuste responsividade

--- Portifolio/src/app/services/menu/menu-service.service.ts
+++ Portifolio/src/app/services/menu/menu-service.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@angular/core';
 import { ButtonsMenu } from '../../models/ButtonsMenu';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
+import { RetornoGlobal } from 'src/app/models/RetornoGlobal';
 
 @Injectable()
 export class MenuServiceService {
 
   constructor(private Client:HttpClient) { }
 
-    getMenuInicial():Observable<ButtonsMenu[]>{
-       return this.Client.get<ButtonsMenu[]>(`${ environment.app_server }/Menu`);
+    getMenuInicial():Observable<RetornoGlobal<ButtonsMenu[]>>{
+       return this.Client.get<RetornoGlobal<ButtonsMenu[]>>(`${ environment.app_server }/Menu`);
     }
 
 
