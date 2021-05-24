6f89535bb957e753400f341d0727b3e2ea627a65
pedrops123 <pedro.furlan1304@hotmail.com>
2021-05-24 18:01:56 -0300

    Redefinicao layout inicial | Comeco layout tooltip

--- Portifolio/src/app/components/tooltip-stack/tooltip-stack.component.ts
+++ Portifolio/src/app/components/tooltip-stack/tooltip-stack.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'tooltip-stack-component',
   templateUrl: './tooltip-stack.component.html',
   styleUrls: ['./tooltip-stack.component.css']
 })
-export class TooltipStackComponent implements OnInit {
+export class TooltipStackComponent implements OnInit , AfterViewInit , OnChanges {
+  
+  @Input('hover') isHover :Boolean;
+  @ViewChild('divMain') _ToolTipMain :ElementRef;
+  
+  constructor(private render:Renderer2) { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.isHover){
+      this.render.setStyle(this._ToolTipMain.nativeElement , "display","block");
+    }
+     else{
+      this.render.setStyle(this._ToolTipMain.nativeElement , "display","none");
+    }
+  }
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    console.log(this._ToolTipMain);
+    this.render.setStyle(this._ToolTipMain.nativeElement , "display","none");
+  }
+
 
   ngOnInit(): void {
   }
--- Portifolio/src/app/pages/initial/initial.component.ts
+++ Portifolio/src/app/pages/initial/initial.component.ts
@@ -7,7 +7,50 @@ import { AppComponent } from 'src/app/app.component';
   styleUrls: ['./initial.component.css']
 })
 export class InitialComponent implements OnInit {
+  stackArray:any[] = [
 
+    {
+      fotoStack:'../../../assets/stacks/angular.png',
+      class: 'base-logo-stack img_angular stack-bottom',
+      id:'img-angular',
+      tooltip:false,
+      descriptionTooltip:'',
+      active:false
+    },
+    {
+      fotoStack:'../../../assets/stacks/dotnet.png',
+      class: 'base-logo-stack img_dotnet stack-up',
+      id:'img-dotnet',
+      tooltip:false,
+      descriptionTooltip:'',
+      active:false
+    },
+    {
+      fotoStack:'../../../assets/foto_perfil/foto_perfil.jpg',
+      class: 'img-foto',
+      id:'',
+      tooltip:false,
+      descriptionTooltip:'',
+      active:false
+    },
+    {
+      fotoStack:'../../../assets/stacks/html5.png',
+      class: 'base-logo-stack img_html stack-up',
+      id:'img-html',
+      tooltip:false,
+      descriptionTooltip:'',
+      active:false
+    },
+    {
+      fotoStack:'../../../assets/stacks/css3.png',
+      class: 'base-logo-stack img_css stack-bottom',
+      id:'img-css',
+      tooltip:false,
+      descriptionTooltip:'',
+      active:true
+    }
+
+  ]; 
   constructor(component:AppComponent) {
       component.setTitle('Pagina Inicial');
    }
